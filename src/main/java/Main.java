4a9e4b59e95c2acd402de8fa75c5b8263f080b55
vsirik <vsirik@niisokb.ru>
2025-01-22 00:25:57 +0700

    single-thread RingBuffer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(5);
+        ringBuffer.add(1);
+        ringBuffer.add(2);
+        ringBuffer.add(3);
+        ringBuffer.add(4);
+        ringBuffer.add(5);
+        ringBuffer.add(6);
+
+        Integer i = ringBuffer.get(0);
+        System.out.println(i);
+
+
+    }
 }
--- src/main/java/RingBuffer.java
+++ src/main/java/RingBuffer.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class RingBuffer {
+import java.util.LinkedList;
+
+public class RingBuffer<T> {
+
+    private Integer indicator;
+    private final int maxSize;
+    private final LinkedList<T> buffer;
+
+    public RingBuffer(int maxSize) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("Max size don't be 0");
+        }
+        this.maxSize = maxSize;
+        this.buffer = new LinkedList<>();
+        this.indicator = 0;
+    }
+
+    public void add(T element) {
+        if (buffer.size() == maxSize) {
+            int localIndicator = (indicator + 1);
+            boolean local = true;
+            while (local) {
+                if ((localIndicator != maxSize)) {
+                    buffer.set(indicator, element);
+                    indicator++;
+                    local = false;
+                } else if ((localIndicator == maxSize)) {
+                    indicator = 0;
+                    localIndicator = indicator;
+                }
+            }
+            return;
+        }
+        buffer.add(element);
+    }
+
+    public T get(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Index don't be < 0");
+        } else if (index >= buffer.size()) {
+            throw new IllegalArgumentException("Index don't be > size");
+        }
+        return buffer.get(index);
+    }
 }
