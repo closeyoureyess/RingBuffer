54e24fee9521cf0113af25776c90feb78ec4d3a3
vsirik <vsirik@niisokb.ru>
2025-01-22 14:59:26 +0700

    multithreading ringbuffer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,32 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Main {
     public static void main(String[] args) {
-        RingBuffer<Integer> ringBuffer = new RingBuffer<>(5);
-        ringBuffer.add(1);
-        ringBuffer.add(2);
-        ringBuffer.add(3);
-        ringBuffer.add(4);
-        ringBuffer.add(5);
-        ringBuffer.add(6);
 
-        Integer i = ringBuffer.get(0);
-        System.out.println(i);
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(12);
+
+        try (ExecutorService executorService = Executors.newFixedThreadPool(20)) {
+
+            executorService.execute(() -> ringBuffer.add(1));
+            executorService.execute(() -> ringBuffer.add(2));
+            executorService.execute(() -> ringBuffer.add(3));
+            executorService.execute(() -> ringBuffer.add(4));
+            executorService.execute(() -> ringBuffer.add(5));
+            executorService.execute(() -> ringBuffer.add(6));
+            executorService.execute(() -> ringBuffer.add(7));
+            executorService.execute(() -> ringBuffer.add(8));
+            executorService.execute(() -> ringBuffer.add(9));
+            executorService.execute(() -> ringBuffer.add(10));
+            executorService.execute(() -> ringBuffer.add(11));
+            executorService.execute(() -> ringBuffer.add(12));
+            executorService.execute(() -> ringBuffer.get(0));
+            executorService.execute(() -> ringBuffer.get(4));
+            executorService.execute(() -> ringBuffer.get(1));
+            executorService.execute(() -> ringBuffer.get(2));
+
+        }
 
 
     }
--- src/main/java/RingBuffer.java
+++ src/main/java/RingBuffer.java
@@ -1,21 +1,25 @@
-import java.util.LinkedList;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class RingBuffer<T> {
 
     private Integer indicator;
     private final int maxSize;
-    private final LinkedList<T> buffer;
+    private final List<T> buffer;
+    private final ReentrantLock reentrantLock;
 
     public RingBuffer(int maxSize) {
         if (maxSize <= 0) {
             throw new IllegalArgumentException("Max size don't be 0");
         }
         this.maxSize = maxSize;
-        this.buffer = new LinkedList<>();
+        this.buffer = Collections.synchronizedList(new ArrayList<>());
         this.indicator = 0;
+        this.reentrantLock = new ReentrantLock();
     }
 
     public void add(T element) {
+        reentrantLock.lock();
         if (buffer.size() == maxSize) {
             int localIndicator = (indicator + 1);
             boolean local = true;
@@ -29,17 +33,24 @@ public class RingBuffer<T> {
                     localIndicator = indicator;
                 }
             }
-            return;
+
+        } else {
+            buffer.add(element);
         }
-        buffer.add(element);
+        reentrantLock.unlock();
     }
 
     public T get(int index) {
-        if (index < 0) {
-            throw new IllegalArgumentException("Index don't be < 0");
-        } else if (index >= buffer.size()) {
-            throw new IllegalArgumentException("Index don't be > size");
+        reentrantLock.lock();
+        try {
+            if (index < 0) {
+                throw new IllegalArgumentException("Index don't be < 0");
+            } else if (index >= buffer.size()) {
+                throw new IllegalArgumentException("Index don't be > size");
+            }
+            return buffer.get(index);
+        } finally {
+            reentrantLock.unlock();
         }
-        return buffer.get(index);
     }
 }
